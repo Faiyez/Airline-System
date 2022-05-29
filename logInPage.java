42e508d3a56550e49f7d524eba78abe4b89fb241
Faiyez <smfnoor@mun.ca>
2022-05-29 14:29:16 -0230

    Added menubar, label bar and new background image

---
+++ logInPage.java
@@ -0,0 +1,82 @@
+import java.util.HashMap;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class logInPage extends JFrame implements ActionListener{
+//    JFrame frame = new JFrame();
+    JButton loginButton = new JButton("Login");
+    JButton resetButton = new JButton("Reset");
+    JTextField idField = new JTextField();
+    JPasswordField passwordField = new JPasswordField();
+    JLabel loginLabel = new JLabel("Login");
+    JLabel passwordLabel = new JLabel("Password");
+    JLabel messageLabel = new JLabel();
+    
+    HashMap<String, String> loginInfo = new HashMap<String, String>();
+    loginPage(HashMap<String, String> realLoginInfo){
+        loginInfo = realLoginInfo;
+        loginLabel.setBounds(50,100,80,20);
+        passwordLabel.setBounds(50,150,80,20);
+        messageLabel.setBounds(120,250,200,100);
+        messageLabel.setFont(new Font(null,Font.BOLD,20));
+        idField.setBounds(120,100,200,20);
+        passwordField.setBounds(120,150,200,20);
+        loginButton.setBounds(140,200,80,20);
+        loginButton.addActionListener(this);
+        resetButton.setBounds(220,200,80,20);
+        resetButton.addActionListener(this);
+        
+
+        add(resetButton);
+        add(loginButton);
+        add(passwordField);
+        add(idField);
+        add(messageLabel);
+        add(loginLabel);
+        add(passwordLabel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,500);
+        setLayout(null);
+        setVisible(true);        
+    }
+
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == resetButton){
+            idField.setText("");
+            passwordField.setText("");
+        }
+        if(e.getSource() == loginButton){
+            String userName = idField.getText();
+            String userPass = String.valueOf(passwordField.getPassword());
+            if(loginInfo.containsKey(userName)){
+                if(loginInfo.get(userName).equals(userPass)){
+                    messageLabel.setForeground(Color.GREEN);
+                    messageLabel.setText("Log in successfull");
+                }
+                else{
+                    messageLabel.setForeground(Color.RED);
+                    messageLabel.setText("Incorrect Password ");                    
+                }
+            }
+            else{
+                messageLabel.setForeground(Color.RED);
+                messageLabel.setText("Wrong Username");
+            }
+        }
+        
+
+    }
+}
+
+
+
+
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    
+}
--- systemFrame.java
+++ systemFrame.java
@@ -1,18 +1,62 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.ImageIcon;
+
+
+import java.awt.Graphics2D;
+//import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
 public class systemFrame extends JFrame implements ActionListener {
-    JLabel headerLabel, passengerHeader, selectionLabel, departLabel, firstLabel, lastLabel, passportLabel, fromLabel, toLabel;
+    JLabel headerLabel, passengerHeader, selectionLabel, 
+    departLabel, firstLabel, lastLabel, passportLabel, fromLabel, 
+    toLabel,imageLabel, headerImage, nameLabel;
     JTextField firstName, lastName, passportNo;
+    JPanel menuPanel, headerPanel;
     JComboBox fromBox, toBox;
+
     systemFrame(){
         //super();
+        //Menu Panel and its components
+        menuPanel = new JPanel();
+        menuPanel.setBackground(Color.WHITE);
+        menuPanel.setBounds(0, 0, 160, 1400);
+        menuPanel.setLayout(null);
+        JButton signInButton = new JButton("Sign In");
+        JButton thingsToDoButton = new JButton("Things To Do");
+        JLabel menuLabel = new JLabel("Menu");
+
+        menuLabel.setBounds(25, 120, 140, 40);
+        menuLabel.setFont(new Font(null ,Font.BOLD,40 ));
+        signInButton.setBounds(20, 190, 120, 40);
+        thingsToDoButton.setBounds(20, 240, 120, 40);
+
+        menuPanel.add(menuLabel);
+        menuPanel.add(signInButton);
+        menuPanel.add(thingsToDoButton);
+
+        headerPanel = new JPanel();
+        headerImage = new JLabel();
+        headerImage.setIcon(new ImageIcon("sky.jpg"));
+        
+        headerPanel.setLayout(null);
+        nameLabel = new JLabel("Airlyne Booking System");
+        nameLabel.setBounds(500,20, 500, 50);
+        nameLabel.setFont(new Font(null, Font.ITALIC, 30));
+        headerPanel.setBounds(0,0,1400,100);
+        headerPanel.add(nameLabel);
+        headerPanel.add(headerImage);
+        
+
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
         String destinations[] = {"Toronto", "Montreal", "Paris", "New York"};
         fromBox = new JComboBox(destinations);
         toBox = new JComboBox(destinations);
         this.setPreferredSize(size);
         //this.setPreferredSize(new Dimension(1200, 600));
+        imageLabel = new JLabel();
+        imageLabel.setIcon(new ImageIcon("world.jpg"));
         fromLabel = new JLabel("From: ");
         toLabel = new JLabel("To: ");
         headerLabel = new JLabel("Airlyne Booking System");
@@ -25,30 +69,35 @@ public class systemFrame extends JFrame implements ActionListener {
         passportNo = new JTextField();
         passportLabel = new JLabel("Passport NO: ");
 
-        toBox.setBounds(480, 130, 100, 20);
-        toLabel.setBounds(420, 130, 100, 20);
+
+        imageLabel.setBounds(0, 0, 1400, 1400);
+        toBox.setBounds(700, 230, 100, 20);
+        toLabel.setBounds(640, 230, 100, 20);
         toLabel.setFont(new Font(null, Font.PLAIN, 15));
-        fromLabel.setBounds(420, 100, 100, 20);
+        fromLabel.setBounds(640, 200, 100, 20);
         fromLabel.setFont(new Font(null, Font.PLAIN, 15));
-        fromBox.setBounds(480, 100, 100 , 20);
-        passportNo.setBounds(120, 160, 100, 20);
-        passportLabel.setBounds(20, 160, 100, 20);
+        fromBox.setBounds(700, 200, 100 , 20);
+        passportNo.setBounds(340, 260, 100, 20);
+        passportLabel.setBounds(240, 260, 100, 20);
         passportLabel.setFont(new Font(null, Font.PLAIN, 15));
-        lastLabel.setBounds(20, 130, 100, 20);
+        lastLabel.setBounds(240, 230, 100, 20);
         lastLabel.setFont(new Font(null, Font.PLAIN, 15));
-        lastName.setBounds(120, 130, 100, 20);
+        lastName.setBounds(340, 230, 100, 20);
         lastName.setFont(new Font(null, Font.PLAIN, 15));
-        firstName.setBounds(120, 100, 100, 20);
-        firstLabel.setBounds(20, 100, 100, 20);
+        firstName.setBounds(340, 200, 100, 20);
+        firstLabel.setBounds(240, 200, 100, 20);
         firstLabel.setFont(new Font(null, Font.PLAIN, 15));
-        headerLabel.setBounds(450, 10, 500, 50);
-        headerLabel.setFont(new Font(null, Font.ITALIC, 30));
-        passengerHeader.setBounds(20, 40, 250, 50);
+        //headerLabel.setBounds(470, 10, 500, 50);
+        //headerLabel.setFont(new Font(null, Font.ITALIC, 30));
+        passengerHeader.setBounds(40, 40, 250, 50);
         passengerHeader.setFont(new Font(null, Font.BOLD, 20));
         //departLabel.setBounds(15, 15, 50, 20);
         //departLabel.setFont(new Font(null, Font.BOLD, 20));
 
         //add(departLabel);
+
+        add(headerPanel);
+        add(menuPanel);
         add(toBox);
         add(toLabel);
         add(fromLabel);
@@ -61,9 +110,11 @@ public class systemFrame extends JFrame implements ActionListener {
         add(firstLabel);
         add(passengerHeader);
         add(headerLabel);
+        add(imageLabel);
         setResizable(true);
         setLayout(null);
         pack();
+        validate();
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
